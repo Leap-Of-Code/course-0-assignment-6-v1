2d16a0bad7b8fb6323b25cfd102a5b6cec764c1d
jinghe315 <63163103+jinghe315@users.noreply.github.com>
2020-05-06 17:14:42 +0000

    Saving progress for all questions.

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -18,26 +18,52 @@ bool Magic(const bool a, const bool b, const bool c, const bool d) {
   if (a && !b && c && d){
     return true;
   }
-  if (a && !b && a && !b){
-    return false 
+  if (a && !b && c && !d){
+    return false; 
   }
-  if (a && !b && !a && b){
-    return true 
+  if (a && !b && !c && d){
+    return true; 
   }
-  if (a && !b && !a && !b){
-    return false 
+  if (a && !b && !c && !d){
+    return false; 
   }
-  if (!a && b && a && b){
-    return true
+  if (!a && b && c && d){
+    return true;
   }
-  if (!a && b && a && !b){
-    return false 
+  if (!a && b && c && !d){
+    return false; 
   }
+  return false;
 }
 
-
-
-int main() {
+int TestMagic(const bool a, const bool b, const bool c, const bool d, const bool expected_answer) {
+  bool answer = Magic(a, b, c, d);
+  if (answer != expected_answer) {
+    cout << "Magic(" << a << ", " << b << ", " << c << ", " << d << ") incorrectly returned " << answer << endl;
+    return 1;
+  }
   return 0;
 }
 
+
+int main() {
+  int num_incorrect = 0;
+  num_incorrect = num_incorrect + TestMagic(true, true, true, true, true);
+  num_incorrect = num_incorrect + TestMagic(true, true, true, false, false);
+  num_incorrect = num_incorrect + TestMagic(true, true, false, true, true);
+  num_incorrect = num_incorrect + TestMagic(true, true, false, false, false);
+  num_incorrect = num_incorrect + TestMagic(true, false, true, true, true);
+  num_incorrect = num_incorrect + TestMagic(true, false, true, false, false);
+  num_incorrect = num_incorrect + TestMagic(true, false, false, true, true);
+  num_incorrect = num_incorrect + TestMagic(true, false, false, false, false);
+  num_incorrect = num_incorrect + TestMagic(false, true, true, true, true);
+  num_incorrect = num_incorrect + TestMagic(false, true, true, false, false);
+
+  if (num_incorrect > 0) {
+    cout << "You got " << num_incorrect << " incorrect!" << endl;
+  } 
+  else {
+    cout << "You got everything right!" << endl;
+  }
+  return 0;
+}

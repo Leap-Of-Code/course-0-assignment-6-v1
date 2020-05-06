9b57b4cc55b268d2f43ab31a2ad98d27db372e84
jinghe315 <63163103+jinghe315@users.noreply.github.com>
2020-05-06 15:41:18 +0000

    Saving progress for all questions.

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -3,19 +3,36 @@
 using namespace std;
 
 bool Magic(const bool a, const bool b, const bool c, const bool d) {
-  if (a || b || c || d){
+  if (a && b && c && d){
     return true;
   }
-  if (a || b || c || !d){
+  if (a && b && c && !d){
     return false; 
   }
-  if (a || b || !c || d){
+  if (a && b && !c && d){
     return true; 
   }
-  if (a || b || !c || !d){
+  if (a && b && !c && !d){
     return false;
   }
-  if (a || !b ||)
+  if (a && !b && c && d){
+    return true;
+  }
+  if (a && !b && a && !b){
+    return false 
+  }
+  if (a && !b && !a && b){
+    return true 
+  }
+  if (a && !b && !a && !b){
+    return false 
+  }
+  if (!a && b && a && b){
+    return true
+  }
+  if (!a && b && a && !b){
+    return false 
+  }
 }
 
 
